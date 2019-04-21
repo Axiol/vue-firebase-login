cf95cca495904c3f83f6e7f79d6370f14a467da1
Arnaud Delante <contact@arnaud.ninja>
2019-04-21 19:22:04 +0200

    Move the Firebase configuration to an env file

--- src/main.js
+++ src/main.js
@@ -7,7 +7,14 @@ Vue.config.productionTip = false;
 
 let app = '';
 
-
+const config = {
+  apiKey: process.env.VUE_APP_FB_APIKEY,
+  authDomain: process.env.VUE_APP_FB_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_FB_DATABASEURL,
+  projectId: process.env.VUE_APP_FB_PROJECTID,
+  storageBucket: process.env.VUE_APP_FB_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGINGSENDERID
+};
 
 firebase.initializeApp(config);
 
