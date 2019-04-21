a966399ca0576ad3355245ff7adc31bb942ef142
Arnaud Delante <contact@arnaud.ninja>
2019-04-21 22:06:26 +0200

    Add Bulma

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -33,31 +33,3 @@
     }
   }
 </script>
-
-<style scoped>
-  .login {
-    margin-top: 40px;
-  }
-
-  input {
-    margin: 10px 0;
-    width: 20%;
-    padding: 15px;
-  }
-
-  button {
-    margin-top: 20px;
-    width: 10%;
-    cursor: pointer;
-  }
-
-  p {
-    margin-top: 40px;
-    font-size: 13px;
-  }
-
-  p a {
-    text-decoration: underline;
-    cursor: pointer;
-  }
-</style>
